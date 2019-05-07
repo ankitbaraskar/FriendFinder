ee0a9d4ab1310550c41ebb71789d1d81845a4072
Kit <46611409+ankitbaraskar@users.noreply.github.com>
2019-05-06 23:09:38 -0400

    add logic to loop through the array and get the min score and hence the matching friend object

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,37 +1,69 @@
 
 
 var friendsArray = [
-{
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
+  {
+    "name": "Ahmed",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
   },
   {
-    "name":"Goat",
-    "photo":"http://earthporm.com/wp-content/uploads/2015/05/goats-licking-salt.png",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
+    "name": "Goat",
+    "photo": "http://earthporm.com/wp-content/uploads/2015/05/goats-licking-salt.png",
+    "scores": [
+      4,
+      2,
+      3,
+      5,
+      4,
+      2,
+      3,
+      4,
+      3,
+      2
+    ]
+  },
+  {
+    "name": "Gangster",
+    "photo": "https://images-na.ssl-images-amazon.com/images/I/618Ze13DXaL._UY550_.jpg",
+    "scores": [
+      2,
+      3,
+      2,
+      5,
+      4,
+      3,
+      1,
+      1,
+      4,
+      2
+    ]
+  },
+  {
+    "name": "Human",
+    "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYDYzUOCEoyduvJkjshVpcxQnR1Y6VAI239mIZ3Vr2367op2ZC",
+    "scores": [
+      4,
+      5,
+      2,
+      5,
+      4,
+      3,
+      3,
+      1,
+      3,
+      5
+    ]
   }
 
 ]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,15 +5,51 @@ var apiRouter = express.Router();
 // data
 var friendsArray = require('../data/friends');
 
+// var scoreArray = friendsArray
+
 // routes
 apiRouter.get('/friends', (req,res)=>{
     res.json(friendsArray);
+    
 });
 
 apiRouter.post('/friends',(req,res)=>{
     friendsArray.push(req.body);
+    loopThroughAllFriends(friendsArray);
+
 });
 
+function sumOfDifferenceBetweenTwoArrays(arrayFromUser,arrayAlreadyExists){
+    var sum=0;
+    for(let i=0;i<arrayFromUser.length;i++){
+        sum += Math.abs(parseInt(arrayFromUser[i])-parseInt(arrayAlreadyExists[i]));
+    }
+    return sum;
+}
+
+function loopThroughAllFriends(arrayOfAllFriends){
+    var placeHolderArray =[];
+    var indexOfLastElement = arrayOfAllFriends.length - 1;
+    for(let j = 0;j<indexOfLastElement;j++){
+        var object = {
+            name:arrayOfAllFriends[j].name,
+            photo:arrayOfAllFriends[j].photo,
+            score: sumOfDifferenceBetweenTwoArrays(arrayOfAllFriends[indexOfLastElement].scores,arrayOfAllFriends[j].scores)
+        };
+        placeHolderArray.push(object);
+    }
+
+    var resultFriendObject = placeHolderArray.hasMin('score');
+    console.log(resultFriendObject);
+}
+
+Array.prototype.hasMin = function(attrib) {
+    return this.reduce(function(prev, curr){ 
+        return prev[attrib] < curr[attrib] ? prev : curr; 
+    });
+ }
+
+ 
 
 // export the router
 module.exports = apiRouter;
